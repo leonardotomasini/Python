7a763bdbab93fd18862f9ce9c7a0d6c5eecff0dc
leonardotomasini <leonardotomasini20@gmail.com>
2025-08-31 20:15:14 -0300

    Add files via upload

---
+++ numero quadrado.py
@@ -0,0 +1,5 @@
+def calcular_quadrado(numero):
+    return numero ** 2
+numero = float(input("digite um numero: "))
+quadrado = calcular_quadrado(numero)
+print(f"o quadrado do {numero} é {quadrado}.")
